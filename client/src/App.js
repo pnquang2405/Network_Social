bbbb5df284641fd21bfe5f62ad6365ed40c03c03
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-11-17 01:35:38 +0700

    setup redux, make login feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Alert from './components/alert/Alert';
 import PageRender from './PageRender'
+import Login from './pages/login'
+import Home from './pages/home'
+import { useEffect } from 'react';
+import { refreshToken } from './redux/actions/authAction'
 
 function App() {
+
+  const { auth } = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(refreshToken())
+  }, [dispatch])
+
   return (
     <Router>
+
+      <Alert />
+
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className='main'>
+          <Route exact path="/" component={auth.token ? Home : Login}></Route>
           <Route exact path="/:page" component={PageRender}></Route>
           <Route exact path="/:page/:id" component={PageRender}></Route>
         </div>
