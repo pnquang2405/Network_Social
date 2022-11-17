287e1684346a45b6ac9572e6ab5548ec58adb79a
Minh Hoàng Võ <vominhhoang12401@gmail.com>
2022-11-18 01:21:49 +0700

    make register, logout and header_UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Alert from './components/alert/Alert';
+
+import Alert from './components/alert/Alert'; 
+import Header from './components/alert/Header';
+
 import PageRender from './PageRender'
 import Login from './pages/login'
 import Home from './pages/home'
+
 import { useEffect } from 'react';
 import { refreshToken } from './redux/actions/authAction'
 
@@ -24,6 +28,7 @@ function App() {
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className='main'>
+          {auth.token && <Header/>}
           <Route exact path="/" component={auth.token ? Home : Login}></Route>
           <Route exact path="/:page" component={PageRender}></Route>
           <Route exact path="/:page/:id" component={PageRender}></Route>
