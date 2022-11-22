e687e92ed1679838196b10a961602ad9804edf0e
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-11-22 14:51:49 +0700

    create post view

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,20 +1,21 @@
 import React from 'react'
 import LoadIcon from '../../images/loading.gif'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import Info from '../../components/profile/Info'
 import Posts from '../../components/profile/Posts'
 
 const Profile = () => {
-  const { profile } = useSelector(state=>state)
-  return(
+  const { profile } = useSelector(state => state)
+  return (
     <div className='profile'>
       {
         profile.loading
-        ?       <img className='d-clock mx-auto my-4' src={LoadIcon} alt="loading" />
-        : <Info />
+          ? <img className='d-clock mx-auto my-4' src={LoadIcon} alt="loading" />
+          : <Info />
       }
 
-      <Posts />
+      {/* <Posts /> */}
     </div>
   )
 }
+export default Profile
