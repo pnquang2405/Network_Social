a11bde8b313c182080490fe7514c3f882da4b76a
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-11-25 16:52:55 +0700

    fix bug display posts in home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -12,9 +12,10 @@ let scroll = 0;
 
 const Home = () => {
     const { homePosts } = useSelector(state => state)
+    console.log("home posts", homePosts)
 
     window.addEventListener('scroll', () => {
-        if(window.location.pathname === '/'){
+        if (window.location.pathname === '/') {
             scroll = window.pageYOffset
             return scroll;
         }
@@ -22,25 +23,27 @@ const Home = () => {
 
     useEffect(() => {
         setTimeout(() => {
-            window.scrollTo({top: scroll, behavior: 'smooth'})
+            window.scrollTo({ top: scroll, behavior: 'smooth' })
         }, 100)
-    },[])
+    }, [])
 
     return (
         <div className="home row mx-0">
             <div className="col-md-8">
                 <Status />
-
                 {
-                    homePosts.loading 
-                    ? <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
-                    : (homePosts.result === 0 && homePosts.posts.length === 0)
-                        ? <h2 className="text-center">No Post</h2>
-                        : <Posts />
+                    console.log(homePosts.result === 0 && homePosts.posts.length === 0)
                 }
-                
+                {
+                    homePosts.loading
+                        ? <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
+                        : (homePosts.result === 0 && homePosts.posts.length === 0)
+                            ? <h2 className="text-center">No Post</h2>
+                            : <Posts />
+                }
+
             </div>
-            
+
             <div className="col-md-4">
                 {/* <RightSideBar /> */}
             </div>
